b1335be4003dd31f9c5a272f65ffabde0b1d9554
Henrique Mendes <henrique.gmendes@hotmail.com>
2019-05-14 14:54:05 -0300

    added new kata

--- kyu-7/Ordered-Count-of-Characters.js
+++ kyu-7/Ordered-Count-of-Characters.js
@@ -6,11 +6,15 @@
 // orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
 
 const orderedCount = (text) => {
+  const array = [];
   const obj = {};
   for (let i = 0; i < text.length; i += 1) {
+    if (array.indexOf(text[i]) === -1) {
+      array.push(text[i]);
+    }
     obj[text[i]] = obj[text[i]] + 1 || 1;
   }
-  return Object.keys(obj).map(e => [e, obj[e]]);
+  return array.map(e => [e, obj[e]]);
 };
 
 console.log(orderedCount('abracadabra'));
